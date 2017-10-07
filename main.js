94b9cca54db45f3c9829859ca04b0dd4cfd24a47
Matheus Diniz Rodrigues <mdrwtf@gmail.com>
2017-10-07 17:24:21 -0300

    Minor Changes
    Clean the Residual Debugging parts and change the name for a more fitting one

--- main.js
+++ main.js
@@ -1,45 +1,51 @@
 window.onload = function(){
     canvas = document.getElementById('screen');
     ctx = canvas.getContext('2d');
-    // ctx.fillStyle="#f8f8f8";
-    // ctx.fillRect(0,0,canvas.width, canvas.height);
     setInterval(draw, 1000/15);
 }
+
+//Variables used to determine the position and size of the line
 var x = 0;
 var y = 0;
 var spc = 20;
 
+
+//Function Called each interval
 function draw(){
+    //call the map function with the variables in it
     var cor = CreateRemap(0, canvas.width, 0, 255);
 
+    //Used the Map function to find numbers between 0 and 255 in order to used them as the RGB values
     var r = Math.floor(cor(x));
     var g = Math.floor(cor(y));
     var b = Math.floor(cor(x+y/2));
     
+    //Line parameters, weight and Color
     ctx.lineWidth=3;
     ctx.strokeStyle='rgb(' + r + ',' + g + ',' + b + ')';
+
+    //Randomizer and the line parameters for printing on the screen
     if(Math.floor(Math.random()*11) < 5){
     ctx.beginPath();
     ctx.moveTo(x, y);
     ctx.lineTo(x + spc, y + spc);
-    
     ctx.stroke();
-    } else{
+    }else{
     ctx.beginPath();
     ctx.moveTo(x, y + spc);
     ctx.lineTo(x + spc, y);
-    // ctx.lineWidth=3;
-    // ctx.strokeStyle='rgb(' + r + ',' + g + ',' + b + ')';
     ctx.stroke();
     }
+
+    //re-set the position of the line 
     x = x + spc;
     if(x > canvas.width){
     x = 0;
     y = y + spc;
     }
-    console.log(r, g, b);
 }
 
+//Map function
 function CreateRemap(inMin, inMax, outMin, outMax) {
     return function remaper(x) {
         return (x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
