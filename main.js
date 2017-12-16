2bea7e648ac2cc9175a7a0c87fa7ea77db9b0d3b
Matheus Diniz Rodrigues <mdrwtf@gmail.com>
2017-12-15 22:53:42 -0200

    10PRINT now takes the whole screen

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 window.onload = function(){
     canvas = document.getElementById('screen');
     ctx = canvas.getContext('2d');
-    setInterval(draw, 1000/15);
+    setInterval(draw, 100/30);
+    canvas.width = window.self.innerWidth;
+    canvas.height = window.self.innerHeight;
 }
 
 //Variables used to determine the position and size of the line
 var x = 0;
 var y = 0;
-var spc = 20;
-
+var spc = parseFloat(Math.floor(window.self.innerWidth / 100));
 
 //Function Called each interval
 function draw(){
@@ -22,7 +23,7 @@ function draw(){
     
     //Line parameters, weight and Color
     ctx.lineWidth=3;
-    ctx.strokeStyle='rgb(' + r + ',' + g + ',' + b + ')';
+    ctx.strokeStyle=`rgb(${r},${g},${b})`;
 
     //Randomizer and the line parameters for printing on the screen
     if(Math.floor(Math.random()*11) < 5){
@@ -50,4 +51,12 @@ function CreateRemap(inMin, inMax, outMin, outMax) {
     return function remaper(x) {
         return (x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
     };
+}
+
+//Make Line function
+function MakeLine(fx, fy, sx, sy){
+    ctx.beginPath();
+    ctx.moveTo(fx, xy);
+    ctx.lineTo(sx, sy);
+    ctx.stroke();
 }
